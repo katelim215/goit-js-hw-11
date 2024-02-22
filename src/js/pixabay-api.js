0a081952b086911cf98c1afe6a36f8f2ac579ce9
katelim215 <katelimosnero215@gmail.com>
2024-02-23 02:23:34 +0800

    updated script

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,13 +2,13 @@ export const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '42488056-39cd93b2543a722d8f6625aa2';
 
 export const options = {
-    params: {
-        key: API_KEY,
-        q: '',
-        image_type: 'photo',
-        orientation: 'horizontal',
-        safesearch: true,
-        page: 1,
-        per_page: 40,
-    }
+  params: {
+    key: API_KEY,
+    q: '',
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    page: 1,
+    per_page: 40,
+  },
 };
